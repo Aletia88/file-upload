12a67676315fac9f4fa1b52f2d72e5c229cf3aa0
Aletia88 <abababmgmgmg@gmail.com>
2024-04-05 20:45:36 +0300

    file upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { FileInput, rem, Table, Button } from '@mantine/core';
+import { CiFileOn } from "react-icons/ci";
+import '@mantine/core/styles.css';
+
+interface UploadedFile {
+  name: string;
+  size: string;
+  date: string;
+}
 
 function App() {
+  const icon = <CiFileOn style={{ width: rem(18), height: rem(18) }} />;
+  
+  const [files, setFiles] = useState<UploadedFile[]>([]);
+
+  const handleRemoveFile = (indexToRemove: number) => {
+    setFiles((prevFiles) => prevFiles.filter((_, index) => index !== indexToRemove));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='w-1/3 m-5'>
+        <FileInput
+          rightSection={icon}
+          label="Upload File"
+          placeholder="Upload File With Lessthan 10mb"
+          rightSectionPointerEvents="none"
+          mt="md"
+          onChange={(payload: File | null) => {
+            if (payload) {
+
+              if (payload.size > 10 * 1024 * 1024) {
+                // File size exceeds the limit, notify the user
+                alert("File size must be less than 10MB");
+                return;
+              }
+              // Handle when a single file is uploaded
+              const file = payload;
+              const processedFile: UploadedFile = {
+                name: file.name,
+                size: `${(file.size / 1000000).toFixed(2)}MB`,
+                date: new Date().toISOString().split('T')[0],
+              };
+              setFiles((prevFiles) => [...prevFiles, processedFile]);
+            } else {
+              // Handle when no file is uploaded
+              console.log("No file uploaded");
+            }
+          }}
+        />
+      </div>
+
+      <Table.ScrollContainer minWidth={500} m="5%">
+        <Table striped>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>File Name</Table.Th>
+              <Table.Th>File Size</Table.Th>
+              <Table.Th>Uploaded Date</Table.Th>
+              <Table.Th></Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>
+            {files.map((file, index) => (
+              <Table.Tr key={index}>
+                <Table.Td>{file.name}</Table.Td>
+                <Table.Td>{file.size}</Table.Td>
+                <Table.Td>{file.date}</Table.Td>
+                <Table.Td>
+                  <Button color='red' onClick={() => handleRemoveFile(index)}>Remove File</Button>
+                </Table.Td>
+              </Table.Tr>
+            ))}
+          </Table.Tbody>
+        </Table>
+      </Table.ScrollContainer>
     </div>
   );
 }
